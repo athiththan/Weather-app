636e5afb9eca2751b81f825be884aec5b01b8a05
Logesh R <48343386+LogeshRavi@users.noreply.github.com>
2021-09-20 18:34:46 +0530

    Update Result.js

--- src/componets/Result.js
+++ src/componets/Result.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Result.css";
 import {Button} from "@material-ui/core";
-const Result = ({temp,desc,icon}) => {
+const Result = ({temp,desc,icon,setTemp}) => {
     return<div className="result">
         <div className="result__box">
             <h1>Temp:{temp}</h1>
             <p>{desc}</p>
-            <img src={icon} alt="logo" />
-            <Button variant="contained" color="default">
+            <img src={icon} alt="logo" /> <br/>
+            <Button variant="contained" color="default" 
+            onClick={()=>{
+               setTemp("")
+            }}>
                 Back
             </Button>
         </div>
